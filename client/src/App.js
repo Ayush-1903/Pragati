b7642eb4822eb284b03f82816cfe52bd4909040a
hokageCV <chaitanyavaru@gmail.com>
2022-12-23 19:01:15 +0530

    test PR - initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import SchoolTodo from './components/dashboard/category/school/todo/SchoolTodo';
 
 function App() {
 
-  const { user } = useAuthContext()
+  const {user} = useAuthContext()
 
   return (
     <BrowserRouter>
