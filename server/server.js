b285eb1288997758f674400ce0c0e25075506024
hokageCV <chaitanyavaru@gmail.com>
2022-12-25 23:43:48 +0530

    auth

--- server/server.js
+++ server/server.js
@@ -1,12 +1,31 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
+
 const app = express();
+const PORT = process.env.PORT || 4000;
+dotenv.config();
 
-const PORT = 4000;
 
-app.get('/', (req, res)=>{
-    res.json("ROLLINGUUUU, THUNDAAAAA" )
-})
+// middleware
+app.use(bodyParser.json({ limit: "10mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(cors());
 
-app.listen(PORT, () => {
-    console.log( `listening on port ${PORT}  ` );
-})
+
+app.use('/user', userRoutes)
+
+
+// connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+         app.listen(PORT, () => {
+            console.log( `listening 🦻 on port ${PORT}!!  ` );
+         })
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
